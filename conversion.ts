0db54c5aa72bea3b4bbf88095926a348e86efbcc
Rahul Vaikunthavasan <rahul_vaikunthavasan@sluz.ch>
2026-05-08 11:23:45 +0200

    Refactoring Übung

--- conversion.ts
+++ conversion.ts
@@ -1,7 +1,7 @@
-export const conversionTable = {
-  league: [3, "miles"],
+export const conversionTable: Record<string, [number, string]> = {
+  league: [3, "mile"],
   mile: [1760, "yard"],
-  // TODO: introduce "furlong" (as 22 yards; equal to 201.168 meters)
+  furlong: [220, "yard"],
   yard: [3, "foot"],
   foot: [12, "inch"],
   inch: [2.54, "centimeter"],
@@ -10,51 +10,18 @@ export const conversionTable = {
 };
 
 export function convertToMeters(value: number, unit: string): number {
-  let miles: number,
-    yards: number,
-    feet: number,
-    inches: number,
-    centimeters: number,
-    meters: number;
-  switch (unit) {
-    case "league":
-      miles = (conversionTable.league[0] as number) * value;
-      yards = (conversionTable.mile[0] as number) * miles;
-      feet = (conversionTable.yard[0] as number) * yards;
-      inches = (conversionTable.foot[0] as number) * feet;
-      centimeters = inches * (conversionTable.inch[0] as number);
-      meters = centimeters * (conversionTable.centimeter[0] as number);
-      break;
-    case "mile":
-      yards = (conversionTable.mile[0] as number) * value;
-      feet = (conversionTable.yard[0] as number) * yards;
-      inches = (conversionTable.foot[0] as number) * feet;
-      centimeters = inches * (conversionTable.inch[0] as number);
-      meters = centimeters * (conversionTable.centimeter[0] as number);
-      break;
-    case "yard":
-      feet = (conversionTable.yard[0] as number) * value;
-      inches = (conversionTable.foot[0] as number) * feet;
-      centimeters = inches * (conversionTable.inch[0] as number);
-      meters = centimeters * (conversionTable.centimeter[0] as number);
-      break;
-    case "foot":
-      inches = (conversionTable.foot[0] as number) * value;
-      centimeters = inches * (conversionTable.inch[0] as number);
-      meters = centimeters * (conversionTable.centimeter[0] as number);
-      break;
-    case "inch":
-      centimeters = (conversionTable.inch[0] as number) * value;
-      meters = centimeters * (conversionTable.centimeter[0] as number);
-      break;
-    case "centimeter":
-      meters = (conversionTable.centimeter[0] as number) * value;
-      break;
-    case "meter":
-      meters = value;
-      break;
-    default:
-      throw new Error(`unknown unit: ${unit}`);
+  while (unit !== "meter") {
+    const entry = conversionTable[unit];
+
+    if (!entry) {
+      throw new Error("Unknown unit: ${unit}");
+    }
+
+    const [factor, nextUnit] = entry;
+
+    value *= factor;
+    unit = nextUnit;
   }
-  return meters;
+
+  return value;
 }
--- conversion_test.ts
+++ conversion_test.ts
@@ -1,6 +1,36 @@
-import { assertAlmostEquals } from "@std/assert";
+import { assertAlmostEquals, assertThrows } from "@std/assert";
 import { convertToMeters } from "./conversion.ts";
 
-Deno.test("imperial units", () => {
+Deno.test("1 mile is 1 meter", () => {
   assertAlmostEquals(convertToMeters(1, "mile"), 1609.344);
 });
+
+Deno.test("1 furlong is 201.168 meter", () => {
+  assertAlmostEquals(convertToMeters(1, "furlong"), 201.168);
+});
+
+Deno.test("1 meter is 1 meter", () => {
+  assertAlmostEquals(convertToMeters(1, "meter"), 1);
+});
+
+Deno.test("1 inch is 0.0254 meters", () => {
+  assertAlmostEquals(convertToMeters(1, "inch"), 0.0254);
+});
+
+Deno.test("1 yard is 0.9144 meters", () => {
+  assertAlmostEquals(convertToMeters(1, "yard"), 0.9144);
+});
+
+Deno.test("1 furlong is 201.168 meters", () => {
+  assertAlmostEquals(convertToMeters(1, "furlong"), 201.168);
+});
+
+Deno.test("100 centimeter is 1 meters", () => {
+  assertAlmostEquals(convertToMeters(100, "centimeter"), 1,)
+})
+
+Deno.test("li ist an unknown unit", () => {
+  assertThrows(() => {
+    convertToMeters(1, "li")
+  }, "Unknown unit: li")
+})
